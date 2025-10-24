74e4178b1f1864e38c8d37885c153d63f91a3877
Dr Bradley Kennedy <bradley.james@me.com>
2025-10-24 16:33:59 +0100

    add aws ses

--- src/lib/mailer.ts
+++ src/lib/mailer.ts
@@ -1,3 +1,28 @@
+import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
+
+const ses = new SESClient({
+  region: process.env.SES_REGION!,
+  credentials: {
+    accessKeyId: process.env.SES_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.SES_SECRET_ACCESS_KEY!,
+  },
+});
+
 export async function sendEmail(to: string, subject: string, message: string) {
-  console.log(`\n📧 [Mock Email]\nTo: ${to}\nSubject: ${subject}\n${message}\n`);
+  if (process.env.NODE_ENV !== "production") {
+    console.log(`\n📧 [Mock Email]\nTo: ${to}\nSubject: ${subject}\n${message}\n`);
+    return;
+  }
+
+  const from = process.env.SES_FROM_ADDRESS!;
+  await ses.send(
+    new SendEmailCommand({
+      Destination: { ToAddresses: [to] },
+      Message: {
+        Body: { Text: { Data: message } },
+        Subject: { Data: subject },
+      },
+      Source: from,
+    })
+  );
 }
