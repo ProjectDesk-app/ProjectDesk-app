36bde6f4f8b28109f06438b0f9a7bfc6596a955c
Dr Bradley Kennedy <bradley.james@me.com>
2025-11-14 12:44:41 +0000

    Remove quotes
    Removed quotes by James Cash Penney and G. Reiss from the quotes list.

--- src/lib/quotes.ts
+++ src/lib/quotes.ts
@@ -12,20 +12,12 @@ export const quotes = [
     author: "Bill Bradley",
   },
   {
-    text: "The best teamwork comes from people who are working independently toward one goal in unison.",
-    author: "James Cash Penney",
-  },
-  {
     text: "The beautiful thing about learning is that no one can take it away from you.",
     author: "B.B. King",
   },
   {
     text: "Alone we can do so little; together we can do so much.",
     author: "Helen Keller",
-  },
-  {
-    text: "Project management is like juggling three balls – time, cost and quality.",
-    author: "G. Reiss",
   },
   {
     text: "Teachers affect eternity; no one can tell where their influence stops.",
