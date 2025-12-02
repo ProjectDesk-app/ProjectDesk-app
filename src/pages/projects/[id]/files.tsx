1ad877258dd9f6cb65f87c067502f0eeee4f8230
Dr Bradley Kennedy <bradley.james@me.com>
2025-12-02 18:08:48 +0000

    refactor: remove redundant ProjectLayout component wrapping in ProjectFiles

--- src/pages/projects/[id]/files.tsx
+++ src/pages/projects/[id]/files.tsx
@@ -194,11 +194,6 @@ export default function ProjectFiles() {
           title={project.title}
           category={project.category}
         >
-        <ProjectLayout
-          projectId={projectId as string}
-          title={project.title}
-          category={project.category}
-        >
           <div className="space-y-6">
             <div className="flex flex-wrap items-center justify-between gap-3">
               <div>
